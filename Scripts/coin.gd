b5e183a9bfaf5d372fd9cd875340a0c071ca109c
Kahils <ksupinski@usv.edu>
2026-05-28 19:37:01 -0700

    5/28

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,12 +1,21 @@
 extends Area2D
 
-
+var player_ref:PlayerBody2D
+@export var speed_boost_value : float = 300
+@onready var speed_buff_timer: Timer = $SpeedBuffTimer
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _on_body_entered(body: Node2D) -> void:
-	var player_ref:PlayerBody2D
 	player_ref = body as PlayerBody2D
 	player_ref.coincount += 1 
 	print(player_ref.coincount)
-	player_ref.speed = 300
+	player_ref.speed = speed_boost_value
+	collision_shape_2d.set_deferred("disabled", true)
+	animated_sprite_2d.visible = false
+	speed_buff_timer.start()
+
+func _on_speed_buff_timer_timeout() -> void:
+	player_ref.speed = 150.0
 	queue_free()
-# queue_free is the same as delete 
+# queue_free is the same as delete
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,6 +6,8 @@ var jump_velocity : float = -300.0
 
 var coincount : int = 0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -19,6 +21,11 @@ func _physics_process(delta: float) -> void:
 	var direction : float = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * speed
+# Flip sprite based on input direction 
+		if direction < 0:
+			animated_sprite_2d.flip_h = true 
+		if direction > 0:
+			animated_sprite_2d.flip_h = false 
 	else:
 		velocity.x = move_toward(velocity.x, 0.0, speed)
 
