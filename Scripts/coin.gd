f5013355e5dc4d0066f61442a9b1ce07cc6f75ff
Kahils <ksupinski@usv.edu>
2026-06-16 15:01:41 -0700

    2dWrapup

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -6,6 +6,8 @@ var player_ref:PlayerBody2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+
+
 func _on_body_entered(body: Node2D) -> void:
 	player_ref = body as PlayerBody2D
 	player_ref.coincount += 1 
--- Scripts/killbox.gd
+++ Scripts/killbox.gd
@@ -1,5 +1,4 @@
 extends Area2D
 
-
 func _on_body_entered(_body: Node2D) -> void:
 	get_tree().reload_current_scene()
